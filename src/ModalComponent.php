61abe736b471dedb0c534266a639c3548bfd3f84
Vincent Sanjaya <42364425+vcnt72@users.noreply.github.com>
2023-10-01 17:18:58 +0700

    Add fit content to max width

--- src/ModalComponent.php
+++ src/ModalComponent.php
@@ -16,15 +16,15 @@ abstract class ModalComponent extends Component implements Contract
 
     protected static array $maxWidths = [
         'sm' => 'sm:max-w-sm',
-        'md' => 'sm:max-w-md',
-        'lg' => 'sm:max-w-md md:max-w-lg',
-        'xl' => 'sm:max-w-md md:max-w-xl',
-        '2xl' => 'sm:max-w-md md:max-w-xl lg:max-w-2xl',
-        '3xl' => 'sm:max-w-md md:max-w-xl lg:max-w-3xl',
-        '4xl' => 'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-4xl',
-        '5xl' => 'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl',
-        '6xl' => 'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-6xl',
-        '7xl' => 'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-7xl',
+        'md' => 'sm:max-w-fit',
+        'lg' => 'sm:max-w-fit md:max-w-lg',
+        'xl' => 'sm:max-w-fit md:max-w-xl',
+        '2xl' => 'sm:max-w-fit md:max-w-xl lg:max-w-2xl',
+        '3xl' => 'sm:max-w-fit md:max-w-xl lg:max-w-3xl',
+        '4xl' => 'sm:max-w-fit md:max-w-xl lg:max-w-3xl xl:max-w-4xl',
+        '5xl' => 'sm:max-w-fit md:max-w-xl lg:max-w-3xl xl:max-w-5xl',
+        '6xl' => 'sm:max-w-fit md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-6xl',
+        '7xl' => 'sm:max-w-fit md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-7xl',
     ];
 
     public function destroySkippedModals(): self
